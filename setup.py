a2442d35dca73fdafb7aa8ca8d35278226436ef1
Jimmy <OpenProfOak@gmail.com>
2018-01-11 12:46:35 -0600

    chore: change package name to match repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autherrors",
+    name="cdiserrors",
     version='0.0.1',
     description="The auth system for the gdcapi.",
     license="Apache",
